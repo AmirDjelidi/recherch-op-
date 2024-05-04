8b624121754ee19d9bb6626a8ddba9556a206565
Amir Djelidi <104193576+AmirDjelidi@users.noreply.github.com>
2024-05-04 15:00:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,76 @@
-def select_file():
-    num = input("Veuillez entrer le numéro du fichier à utiliser (1-12)\n0 pour quitter: ")
-    file_path = f'probleme {num}.txt'
-    if num == '0':
-        print("Au revoir!")
-        exit()
-    else:
-        if num not in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']:
-            print("Numéro invalide!")
-            file_path = select_file()
-        return file_path
-    
-# Jsp comment lire les fichier de facon optimal pour la suite
-def lire_probleme_transport(fichier):
-    with open(fichier, 'r') as f:
-        lignes = f.readlines()
-    # C'est la première ligne qui contient les valeurs de n et m donc celle là elle est bonne mais
-    n, m = map(int, lignes[0].split())
-    # Mais ici je sais pas comment séparé les ligne pour que ça soit utile plus tard
-    provisions = []
-    for i in range(1, n+1):
-        provisions.append(list(map(float, lignes[i].split()[:-1])))
-    
-    commandes = list(map(float, lignes[n+1].split()))
-
-    return n, m, provisions, commandes
+def coin_nord_ouest(offre, demande):
 
 
-def main():
-    while True:
-        file_path = select_file()
-        n, m, provisions, commandes = lire_probleme_transport(file_path)
-        print("Nombre de fournisseurs:", n)
-        print("Nombre de clients:", m)
-        print("Provisions:", provisions)
-        print("Commandes:", commandes)
+    solution = [[0] * len(demande) for _ in range(len(offre))]
 
 
-if __name__ == '__main__':
-    main()
+    i, j = 0, 0
+
+
+    while i < len(offre) and j < len(demande):
+
+        quantite_allouee = min(offre[i], demande[j])
+        solution[i][j] = quantite_allouee
+
+
+        offre[i] -= quantite_allouee
+        demande[j] -= quantite_allouee
+
+
+        if offre[i] == 0:
+            i += 1
+        if demande[j] == 0:
+            j += 1
+
+
+
+
+    return solution
+
+def lire_fichier_complet(nom_fichier):
+    try:
+        with open(nom_fichier, 'r') as fichier:
+            lignes = fichier.readlines()
+
+        # Première ligne pour les dimensions, pas nécessaire pour le parsing puisque le fichier contient directement les données
+        nb_lignes = int(lignes[0].split()[1])
+
+        matrice_couts = []
+        offres = []
+
+        # Lecture des coûts et des offres
+        for ligne in lignes[1:1 + nb_lignes]:
+            elements = list(map(int, ligne.split()))
+            matrice_couts.append(elements[:-1])  # Tous sauf le dernier élément
+            offres.append(elements[-1])  # Dernier élément
+
+        # Lecture des demandes, qui sont sur la dernière ligne après les coûts et offres
+        demandes = list(map(int, lignes[1 + nb_lignes].split()))
+
+        return matrice_couts, offres, demandes
+
+    except FileNotFoundError:
+        print(f"Le fichier {nom_fichier} n'existe pas.")
+        return None, None, None
+    except Exception as e:
+        print(f"Une erreur est survenue: {e}")
+        return None, None, None
+
+# Exemple d'utilisation
+matrice_couts, offres, demandes = lire_fichier_complet('problems/probleme 13.txt')
+if matrice_couts is not None:
+    print("Matrice des coûts:", matrice_couts)
+    print("Offres:", offres)
+    print("Demandes:", demandes)
+
+
+'''
+costs = [[30, 20, 20],[10, 50, 20],[50, 40, 30]]
+offre = [450, 600, 350]
+demande = [500, 600, 300]
+'''
+solution = coin_nord_ouest(offres, demandes)
+
+print("Solution initiale:")
+for row in solution:
+    print(row)
